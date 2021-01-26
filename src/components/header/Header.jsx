d18234fdd27c55dfda57fd0b92fe4ae837b6140a
Developer genius <developer_genius@protonmail.com>
2021-01-26 09:27:29 -0500

    data rendered

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ function Header(props) {
     return(
         <div className="header">
             <div>
-                <h2>Snap-Shot</h2>
+                <h2>Snapa-Shot</h2>
             </div>
         </div>
 
